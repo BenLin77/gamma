8fb718700c5c416d643b796c10ac5b733843636e
BenLin77 <goldking521@email.com>
2025-03-20 17:24:02 +0800

    reverse run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ cd /home/ben/code/gex
 sleep 60
 /home/ben/.local/bin/pipenv run python playwright_record.py --auth ming_auth.json  --config config_ming.json >> /home/ben/Downloads/crontab.log 2>&1
 sleep 10
-/home/ben/.local/bin/pipenv run python gamma_converter.py  >> /home/ben/Downloads/crontab.log 2>&1
-sleep 600
+/home/ben/.local/bin/pipenv run python gamma_converter.py --overwrite  >> /home/ben/Downloads/crontab.log 2>&1
+sleep 150
 /home/ben/.local/bin/pipenv run python sending_discord.py >> /home/ben/Downloads/crontab.log 2>&1
 sleep 15
 /home/ben/.local/bin/pipenv run python put_dom_trade.py >> /home/ben/Downloads/crontab.log 2>&1
